d5f9a95d00cde1d9f2c97dc69814cfd471b61f62
EliOA99 <149865700+EliOA99@users.noreply.github.com>
2023-11-21 16:50:11 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,13 +1,29 @@
 import random
 
+def choose_random_word(word_list):
+    return random.choice(word_list)
+
+def get_user_input():
+    return input("Enter a single letter: ")
+
+def is_valid_guess(guess):
+    return len(guess) == 1 and guess.isalpha()
+
+def main():
+
 favorite_fruits = ["Grapes", "Apple", "Orange", "Pineapple", "Banana"]
 
 word_list = favorite_fruits
 
 word = random.choice(word_list)
-
 print("Randomly chosen word:", word)
 
 guess = input("Enter a single letter: ")
 
-print("User's guess:", guess)
+ if is_valid_guess(guess):
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
+
+if __name__ == "__main__":
+    main()
