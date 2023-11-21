a85fa26dc2cd9ec628b845d208c8a96d68852497
Your Name <youremail@yourdomain.com>
2023-11-21 20:16:04 +0000

    Fin Fin

--- milestone_3.py
+++ milestone_3.py
@@ -1,47 +1,42 @@
 import random
 
 def choose_random_word(word_list):
-    return random.choice(word_list)
+    if not word_list:
+        return None
+    return word_list.pop(random.randrange(len(word_list)))
 
-def get_user_input():
-    return input("Enter a single letter: ")
+def get_valid_user_input():
+    while True:
+        guess = input("Enter a letter: ")
+        if is_valid_guess(guess):
+            return guess.lower()
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
 
 def is_valid_guess(guess):
     return len(guess) == 1 and guess.isalpha()
 
 def check_guess(word, guess):
-    # Step 2: Convert the guess into lower case
-    guess = guess.lower()
-
-    # Step 3: Check if the guess is in the word
     if guess in word.lower():
-        # Print a message for a correct guess
         print(f"Good guess! '{guess}' is in the word.")
         return True
     else:
-        # Print a message for an incorrect guess
-        print(f"Sorry, '{guess}' is not in the word. Try again.")
+        print(f"Sorry, '{guess}' is not in the list. Please try again.")
         return False
 
-def ask_for_input(word):
+def play_game():
+    favorite_fruits = ["Grapes", "Apple", "Orange", "Pineapple", "Banana"]
+    
     while True:
-        # Step 2: Move the code to check if the guess is in the word into this function
-        guess = get_user_input()
+        word = choose_random_word(favorite_fruits)
+        if word is None:
+            print("There are no more words to guess. Game over.")
+            break
 
-        # Step 3: Call the check_guess function to check if the guess is in the word
-        if is_valid_guess(guess):
+        while True:
+            guess = get_valid_user_input()
             if check_guess(word, guess):
                 break
-        else:
-            # Print an error message for an invalid guess
-            print("Invalid letter. Please, enter a single alphabetical character.")
-
-def main():
-    favorite_fruits = ["Grapes", "Apple", "Orange", "Pineapple", "Banana"]
-    word = choose_random_word(favorite_fruits)
-
-    # Step 4: Call the ask_for_input function to test the code
-    ask_for_input(word)
 
 if __name__ == "__main__":
-    main()
+    play_game()
---
+++ milestone_4.py
@@ -0,0 +1,65 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+        self.word = self._choose_random_word()
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+
+    def _choose_random_word(self):
+        return random.choice(self.word_list).lower()
+
+    def _replace_letters_in_word_guessed(self, guess):
+        guess = guess.lower()
+        for i, letter in enumerate(self.word):
+            if letter == guess:
+                self.word_guessed[i] = guess
+                self.num_letters -= 1
+
+    def _print_correct_guess_message(self, guess):
+        print(f"Good guess! '{guess}' is in the word.")
+
+    def _print_incorrect_guess_message(self, guess):
+        self.num_lives -= 1
+        print(f"Sorry, '{guess}' is not in the word.")
+        print(f"You have {self.num_lives} lives left.")
+
+    def _is_valid_guess(self, guess):
+        return len(guess) == 1 and guess.isalpha()
+
+    def _is_repeated_guess(self, guess):
+        return guess in self.list_of_guesses
+
+    def check_guess(self, guess):
+        if guess in self.word:
+            self._print_correct_guess_message(guess)
+            self._replace_letters_in_word_guessed(guess)
+            return True
+        else:
+            self._print_incorrect_guess_message(guess)
+            return False
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Enter a letter: ")
+
+            if not self._is_valid_guess(guess):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif self._is_repeated_guess(guess):
+                print("You already tried that letter!")
+            else:
+                if self.check_guess(guess):
+                    break
+
+            self.list_of_guesses.append(guess)
+
+if __name__ == "__main__":
+    hangman_game = Hangman(["Grapes", "Apple", "Orange", "Pineapple", "Banana"])
+    hangman_game.ask_for_input()
+
+    print(f"Word Guessed: {' '.join(hangman_game.word_guessed)}")
+    print(f"Number of Letters: {hangman_game.num_letters}")
+    print(f"Number of Lives: {hangman_game.num_lives}")
