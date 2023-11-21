a7dada37a36c3b105b155ad42bfd3a3e164bbad4
EliOA99 <149865700+EliOA99@users.noreply.github.com>
2023-11-21 17:11:28 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,10 @@
+import random
 
+# Step 2: Create a list containing the names of your 5 favorite fruits.
 favorite_fruits = ["Grapes", "Apple", "Orange", "Pineapple", "Banana"]
 
 word_list = favorite_fruits
 
-print("Word List:", word_list)
+word = random.choice(word_list)
+
+print("Random Word:", word)
