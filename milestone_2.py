88e31ce932fb8a71f0936c190733ac5939efac77
EliOA99 <149865700+EliOA99@users.noreply.github.com>
2023-11-21 17:20:26 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,10 +1,29 @@
 import random
 
-# Step 2: Create a list containing the names of your 5 favorite fruits.
-favorite_fruits = ["Grapes", "Apple", "Orange", "Pineapple", "Banana"]
+def choose_random_word(word_list):
+    return random.choice(word_list)
 
-word_list = favorite_fruits
+def get_user_input():
+    return input("Enter a single letter: ")
 
-word = random.choice(word_list)
+def is_valid_guess(guess):
+    return len(guess) == 1 and guess.isalpha()
 
-print("Random Word:", word)
+def main():
+    favorite_fruits = ["Grapes", "Apple", "Orange", "Pineapple", "Banana"]
+
+    word_list = favorite_fruits
+
+    word = choose_random_word(word_list)
+    print("Random Word:", word)
+
+    guess = get_user_input()
+
+    if is_valid_guess(guess):
+        # Print a message for a valid input
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
+
+if __name__ == "__main__":
+    main()
