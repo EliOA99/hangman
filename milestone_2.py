5229dc5541674cf1b94b7256bfa8b41e874bd2fa
EliOA99 <149865700+EliOA99@users.noreply.github.com>
2023-11-21 14:20:30 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,15 @@
+import random
 
 favorite_fruits = ["Grapes", "Apple", "Orange", "Pineapple", "Banana"]
 
-
 word_list = favorite_fruits
 
+word = random.choice(word_list)
 
-print(word_list)
+print(word)
+
+guess = input("Enter a single letter: ")
+
+print("You guessed:", guess)
+
+user_input = input("Enter a single letter: ")
